8b1389460bbba2037b88ba119da2f6b59eac906f
Fred Wong <fredwong.it@gmail.com>
2017-12-21 17:32:57 -0500

    Fetching Data

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,4 +1,8 @@
-const MongoClient = require('mongodb').MongoClient;
+//const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
+
+// var obj = new ObjectID();
+// console.log(obj);
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     if (err) {
@@ -18,15 +22,18 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     //     console.log(JSON.stringify(result.ops, undefined, 2))
     // });
 
+    // Insert new doc into Users (name, age, location)
     db.collection('Users').insertOne({
         name: 'Fred',
         age: 24,
         location: 'Stouffville'
     }, (err, result) => {
-        
-    })
+        if (err) {
+            return console.log('Unable to insert todo', err);
+        }
 
-    // Insert new doc into Users (name, age, location)
+        console.log(result.ops[0]._id.getTimestamp()) 
+    })
 
     db.close();
 });
