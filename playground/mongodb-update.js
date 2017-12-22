dc7e31c73f7529887e1ea0e3c74b03c5ef231801
Fred Wong <fredwong.it@gmail.com>
2017-12-22 13:37:08 -0500

    Updating Data

---
+++ playground/mongodb-update.js
@@ -0,0 +1,43 @@
+//const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
+
+// var obj = new ObjectID();
+// console.log(obj);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+
+    console.log('Connected to MongoDB server');
+
+    // findOneAndDelete
+    db.collection('Todos').findOneAndUpdate({ 
+        _id: new ObjectID('5a3d21a3a5ae1d01de2f89fa') 
+    }, {
+        $set: {
+            completed: true
+        }
+    }, {
+        returnOriginal: false
+    }).then((result) => {
+        console.log(result);
+    });
+
+    db.collection('Users').findOneAndUpdate({ 
+        _id: new ObjectID('5a3c283d904f3b74f436058c')
+    }, {
+        $set: {
+            name: 'Fred1'
+        },
+        $inc: {
+            age: 1
+        }
+    }, {
+        returnOriginal: false
+    }).then((result) => {
+        console.log(result);
+    });
+
+    //db.close();
+});
