244847f7918b080b765595ba9cd4df4340810b8f
Fred Wong <fredwong.it@gmail.com>
2017-12-27 17:04:33 -0500

    Test POSt /todos route

--- server/server.js
+++ server/server.js
@@ -12,7 +12,6 @@ app.use(bodyParser.json());
 
 // config route
 app.post('/todos', (req, res) => {
-    console.log(req.body)
     var todo = new Todo({
         text: req.body.text
     });
@@ -28,38 +27,6 @@ app.listen(3000, () => {
     console.log('Started on port 3000');
 });
 
-
-
-
-
-// var newTodo = new Todo({
-//     text: 'Cook dinner'
-// });
-
-// newTodo.save().then((doc) => {
-//     console.log('Saved todo', doc)
-// }, (e) => {
-//     console.log('Unable to save todo')
-// });
-
-// var otherTodo = new Todo({
-//     text: 'eat dinner',
-//     completed: true,
-//     completedAt: 123
-// });
-
-// otherTodo.save().then((doc) => {
-//     console.log('Saved todo', doc)
-// }, (e) => {
-//     console.log('Unable to save todo')
-// });
-
-// var newUser = new User({
-//     email: 'abc@gmail.com'
-// });
-
-// newUser.save().then((doc) => {
-//     console.log('Saved user', doc)
-// }, (e) => {
-//     console.log('Unable to save todo')
-// });
+module.exports = {
+    app
+};
