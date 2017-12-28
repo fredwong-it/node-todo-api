3ce809b10e80df74f7da04360859bfca51ca3686
Fred Wong <fredwong.it@gmail.com>
2017-12-28 14:13:32 -0500

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ var app = express();
 app.use(bodyParser.json());
 
 // config route
+// POST
 app.post('/todos', (req, res) => {
     var todo = new Todo({
         text: req.body.text
@@ -18,6 +19,17 @@ app.post('/todos', (req, res) => {
 
     todo.save().then((doc) => {
         res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+// GET
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({
+           todos 
+        });
     }, (e) => {
         res.status(400).send(e);
     });
