2089a74f08f8498364261e5cc7bfbae65db2f02b
Fred Wong <fredwong.it@gmail.com>
2018-01-02 16:11:20 -0500

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
@@ -32,6 +33,33 @@ app.get('/todos', (req, res) => {
         });
     }, (e) => {
         res.status(400).send(e);
+    });
+});
+
+// GET /todos/123456
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    // valid id using isValid
+        // 404 - send back empty send
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    // findById
+        // success
+            // if todo - send it back
+            // if no todo - send back 404 with empty body
+        // error
+            // 400 - and send empty body back
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({ todo });
+    }).catch((e) => {
+        res.status(400).send();
     });
 });
 
