cab733e57a8aac0bb4b469113173b300437712f2
Fred Wong <fredwong.it@gmail.com>
2018-01-02 11:03:16 -0500

    Add queries playground file

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,28 @@
+const { ObjectID } = require('mongodb');
+const { mongoose } = require('./../server/db/mongoose');
+const { Todo } = require('./../server/models/todo');
+
+var id = '5a4544e4c9d830203803ca3911';
+
+if (!ObjectID.isValid(id)) {
+    return console.log('ID not valid');
+}
+
+// Todo.find({
+//     _id: id
+// }).then((todos) => {
+//     console.log('Todos', todos);
+// });
+
+// Todo.findOne({
+//     _id: id
+// }).then((todo) => {
+//     console.log('Todo', todo);
+// });
+
+Todo.findById(id).then((todo) => {
+    if (!todo) {
+        return console.log('Id not found');
+    }
+    console.log('Todo By Id', todo);
+}).catch((e) => console.log(e));
