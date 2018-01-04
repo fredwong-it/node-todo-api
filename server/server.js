f7797ab6ab52e59a6dada8f120946ae9e03d5ed8
Fred Wong <fredwong.it@gmail.com>
2018-01-04 12:17:05 -0500

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
 
 var { mongoose } = require('./db/mongoose');
@@ -55,7 +56,7 @@ app.get('/todos/:id', (req, res) => {
     });
 });
 
-// Delete
+// DELETE
 app.delete('/todos/:id', (req, res) => {
     var id = req.params.id;
 
@@ -70,6 +71,33 @@ app.delete('/todos/:id', (req, res) => {
     }).catch((e) => {
         res.status(400).send();
     });
+});
+
+// PATCH, update a resource
+app.patch('/todos/:id', (req, res) => {
+     const id = req.params.id;
+     var body = _.pick(req.body, ['text', 'completed']);
+
+     if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();        // javascript timestamp
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, { $set: body }, { new: true }).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({ todo });
+    }).catch((e) => {
+        res.status(400).send();
+    })
 });
 
 app.listen(3000, () => {
