a210f8feab7d5b44400594a4f13a271589ba818c
Fred Wong <fredwong.it@gmail.com>
2018-01-12 17:23:39 -0500

    Add POST /users route and token method

--- server/models/user.js
+++ server/models/user.js
@@ -1,15 +1,66 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-// User
-// email - require it - trim it - set type - set min length of 1
-var User = mongoose.model('User', {
+var UserSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
         minlength: 1,
-        trim: true
-    }
+        trim: true,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: '{VALUE} is not a valid email'
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
+
+UserSchema.methods.toJSON = function () {
+    var user = this;
+    var userObject = user.toObject();       // only properties available on the document
+
+    return _.pick(userObject, ['_id', 'email']);
+};
+
+// add instance method
+// => function do not bind a this keyword, so we use regular function
+UserSchema.methods.generateAuthToken = function () {
+    var user = this;
+    var access = 'auth';
+    var token = jwt.sign({ _id: user._id.toHexString(), access }, 'abc123').toString();
+
+    // don't know why this didn't work
+    // user.tokens.push({
+    //     access,
+    //     token
+    // });
+    user.tokens = user.tokens.concat([{access, token}]);
+
+    return user.save().then(() => {
+        return token;
+    });
+};
+
+// User
+// email - require it - trim it - set type - set min length of 1
+var User = mongoose.model('User', UserSchema);
 
 module.exports = {
     User
