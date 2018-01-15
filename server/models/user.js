7c0bf7a277b50e7ad63630b9296ebff18fd0e7af
Fred Wong <fredwong.it@gmail.com>
2018-01-15 10:45:29 -0500

    Add GET /users/me and authenticate middleware

--- server/models/user.js
+++ server/models/user.js
@@ -58,6 +58,26 @@ UserSchema.methods.generateAuthToken = function () {
     });
 };
 
+UserSchema.statics.findByToken = function (token) {
+    var User = this;
+    var decoded;
+
+    try {
+        decoded = jwt.verify(token, 'abc123');
+    } catch (e) {
+        // return new Promise((resolve, reject) => {
+        //     reject();
+        // });
+        return Promise.reject();
+    }
+
+    return User.findOne({
+        '_id': decoded._id,
+        'tokens.token': token,
+        'tokens.access': 'auth'
+    });
+}
+
 // User
 // email - require it - trim it - set type - set min length of 1
 var User = mongoose.model('User', UserSchema);
