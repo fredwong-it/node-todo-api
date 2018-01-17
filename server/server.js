3ca8e09be6b0e204a57827be9016f81eae8ebf3e
Fred Wong <fredwong.it@gmail.com>
2018-01-17 17:40:20 -0500

    Add POSt /users/login route

--- server/server.js
+++ server/server.js
@@ -118,6 +118,23 @@ app.post('/users', (req, res) => {
     });
 });
 
+// POST /users/login
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+
+    var user = User.findOne({ 
+        email: body.email
+    });
+
+    User.findByCredentials(body.email, body.password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user); 
+        });
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
+
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
 });
